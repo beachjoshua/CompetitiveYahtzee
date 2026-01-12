3e6bd4bfe8c4e8f05a3b9641b4e6360edf0282ad
beachjoshua <beachtjosh@gmail.com>
2026-01-12 13:32:19 -0600

    now host is logged and can start game

--- app.py
+++ app.py
@@ -21,7 +21,9 @@ def index():
 def create_room():
     code = generate_code()
     rooms[code] = {
-        "players": []
+        "host": "socket_id_placeholder",
+        "players": [],
+        "started": False
     }
     return jsonify({"code": code})
 
@@ -43,9 +45,56 @@ def handle_join(data):
         return
 
     join_room(code)
-    rooms[code]["players"].append(name)
+    
+    if len(rooms[code]["players"]) == 0:
+        rooms[code]["host"] = request.sid
+    
+    rooms[code]["players"].append({
+        "name": name,
+        "sid": request.sid
+    })
 
-    emit("room_update", rooms[code]["players"], room=code)
+    emit("room_update", {
+        "players": [p["name"] for p in rooms[code]["players"]],
+        "is_host": request.sid == rooms[code]["host"],
+        "started": rooms[code].get("started", False)
+    }, room=request.sid)
+
+    emit("player_list", 
+         [p["name"] for p in rooms[code]["players"]],
+         room=code)
+
+@socketio.on("start_game")
+def start_game(data):
+    code = data["code"]
+
+    if code not in rooms:
+        return
+
+    # SECURITY CHECK
+    if request.sid != rooms[code]["host"]:
+        emit("error", "Only the host can start the game")
+        return
+
+    rooms[code]["started"] = True
+    emit("game_started", room=code)
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    for code, room in rooms.items():
+        for p in room["players"]:
+            if p["sid"] == request.sid:
+                room["players"].remove(p)
+
+                # Transfer host
+                if room["host"] == request.sid and room["players"]:
+                    room["host"] = room["players"][0]["sid"]
+
+                emit("player_list",
+                     [x["name"] for x in room["players"]],
+                     room=code)
+                return
+
 
 @socketio.on("leave_room")
 def handle_leave(data):
@@ -57,9 +106,6 @@ def handle_leave(data):
         leave_room(code)
         emit("room_update", rooms[code]["players"], room=code)
 
-@socketio.on("disconnect")
-def handle_disconnect():
-    print("User disconnected")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
